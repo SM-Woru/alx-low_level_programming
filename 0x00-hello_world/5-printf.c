85b338d4963a147cc862e97c319341e5a0f7ff9a
Your Name <you@example.com>
2022-09-10 20:45:30 -0700

    using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry function
+ *
+ * printf - prints string
+ *
+ * return: 0 - exit function, always return zero
+ */
+
+int main(void)
+{
+  printf("with proper grammar, but the outcome is a piece of art,");
+
+  return (0);
+}

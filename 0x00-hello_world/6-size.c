32d9d2f931e6e2221dc39751363ef746a558912b
Your Name <you@example.com>
2022-09-10 21:40:38 -0700

    data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,11 +9,11 @@ int main()
   long long int longlongType;
   float floatType;
 
-  printf("size of a char: %ld byte(s)\n",sizeof(charType));
-  printf("size of an int: %ld byte(s)\n",sizeof(integerType));
-  printf("size of a long int: %ld byte(s)\n",sizeof(longType));
-  printf("size of a long long int: %ld byte(s)\n",sizeof(longlongType));
-  printf("size of a float: %ld byte(s)\n",sizeof(floatType));
+  printf("Size of a char: %ld byte(s)\n",sizeof(charType));
+  printf("Size of an int: %ld byte(s)\n",sizeof(integerType));
+  printf("Size of a long int: %ld byte(s)\n",sizeof(longType));
+  printf("Size of a long long int: %ld byte(s)\n",sizeof(longlongType));
+  printf("Size of a float: %ld byte(s)\n",sizeof(floatType));
 
   return (0);
 }

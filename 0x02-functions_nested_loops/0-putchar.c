8d819649250dc40812235d7f8642c6f5e0cff2de
Your Name <you@example.com>
2022-09-13 21:19:01 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,19 @@
-#include <stdio.h>
-
-
-int main()
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-  printf("_putchar\n");
-    return 0;
+  char *ch = "Holberton";
+
+  while (*ch)
+    {
+      _putchar(*ch);
+      ch++;
+    }
+  _putchar('\n');
+
+  return (0);
 }

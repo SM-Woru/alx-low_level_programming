9bc9de366d279d93900a4c8f81cbce45abef4e35
Your Name <you@example.com>
2022-09-14 21:25:10 -0700

    7,8,9,10,11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints all natural numbers from input to 98
+ */
+
+void print_to_98(int i)
+{
+  if (i >= 98)
+    {
+      while (i > 98)
+	printf("%d, ", i--);
+      printf("%d\n", i);
+    }
+  else
+    {
+      while (i < 98)
+	printf("%d, ", ++);
+      printf("%d\n", i);
+    }
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_last_digit - a function that prints the last digit of a number.
+ *
+ * Return: Value of the last digit.
+ */
+
+int print_last_digit(int i)
+
+{
+  int last_digit = i % 10;
+
+  if (last_digit < 0)
+    last_digit *= -1;
+
+  _putchar(last_digit + '0');
+
+  return (last_digit);
+}

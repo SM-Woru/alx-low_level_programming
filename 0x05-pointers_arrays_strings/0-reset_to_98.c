69fadf3496c2cb29b1fbb1ba74096686d3d87fbd
Your Name <you@example.com>
2022-09-20 13:05:31 -0700

    reset pointer

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,23 +1,12 @@
 #include "main.h"
 
 /**
- * main - a function that updates the value of pointer
+ * void reset_to_98 - a function that updates the value of a pointer
  * 
  * *n - pointer variable
- *
- * m - variable of type integer
- *
- * return: 0
  */
 
-void main()
-{
-  int m;
-  int *n;
-
-  m = 402;
-  n = &m;
-  
   void reset_to_98(int *n);
-  *n = 98;
-}
+  {
+    *n = 98;
+  }

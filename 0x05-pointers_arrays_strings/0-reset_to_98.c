b0f5c9fe4f3e92f3bf910bc4cd71cc58de6384b5
Your Name <you@example.com>
2022-09-20 13:36:33 -0700

    swaps

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -6,7 +6,7 @@
  * *n - pointer variable
  */
 
-  void reset_to_98(int *n);
-  {
-    *n = 98;
-  }
+void reset_to_98(int *n)
+{
+  *n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/1_swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the value of two integers
+ *
+ * @a - first integer
+ * 
+ * @b - seconf integer
+ */
+
+void swap_int(int *a, int *b);
+
+{
+  int t = *a;
+    *a = *b;
+    *b = t;
+}

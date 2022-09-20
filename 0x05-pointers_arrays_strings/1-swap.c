6d33cc96c5c5d3e7db892287a3c6cc10e12a0dd8
Your Name <you@example.com>
2022-09-20 13:55:17 -0700

    swaps

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int -  function that swaps two integers
+ *
+ *@a - first integer
+ * 
+ *@b - second integer
+ */
+
+void swap_int(int *a, int *b);
+
+{
+  int t = *a;
+  *a = *b;
+  *b = t;
+}
